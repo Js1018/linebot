ca966f7ba990bc2eb90ab919bc446dbc0f10ff3f
Js <114293615+Js1018@users.noreply.github.com>
2022-11-04 09:18:56 +0800

    Update wsgi.py

--- linebot_tist/mylinebot/wsgi.py
+++ linebot_tist/mylinebot/wsgi.py
@@ -6,7 +6,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
-
+import sys
+path = '/mylinebot'
+if path not in sys.path:
+    sys.path.append(path)
 import os
 
 from django.core.wsgi import get_wsgi_application
